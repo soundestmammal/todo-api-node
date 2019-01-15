7d7b4cbd4a1bd4e357df327758fffae845f9d5f3
Robert Checco <robert.checco@gmail.com>
2019-01-15 13:18:51 -0500

    Connect to MongoDB database from Node application

--- playgrounds/mongodb-connect.js
+++ playgrounds/mongodb-connect.js
@@ -1,5 +1,6 @@
 const { MongoClient, ObjectID } = require('mongodb');
 
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
         return console.log('Unable to connect to mongo db server');
@@ -10,10 +11,10 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     // }, (err) => {
     //     console.log('Unable to fetch count', err);
     // });
-    db.collection('Users').find({name: 'Robert Checco'}).toArray().then((docs) => {
-        console.log(JSON.stringify(docs, undefined, 2));
-    }, (err) => {
-        return console.log('Count not find a user with that name');
-    })
-    // db.close();
+    // db.collection('Users').find({name: 'Robert Checco'}).toArray().then((docs) => {
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     return console.log('Count not find a user with that name');
+    // })
+    db.close();
 });
