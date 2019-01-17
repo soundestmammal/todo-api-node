3ed07409053629c5f8bc6e111301d043d2deb728
Robert Checco <robert.checco@gmail.com>
2019-01-16 23:59:10 -0500

    Todo list

--- playgrounds/mongodb-connect.js
+++ playgrounds/mongodb-connect.js
@@ -6,33 +6,43 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
         return console.log('Unable to connect to mongo db server');
     }
     console.log('Connected to the MongoDB server');
-    db.collection('Todos').insertOne({
-        text: 'Charge apple watch',
-        completed: false
-    },(err, result) => {
-        if (err) {
-            return console.log('There was an error adding the todo', err);
-        }
-    console.log(JSON.stringify(result.ops, undefined, 2));
-    });
-    db.collection('Users').insertOne({
-        name: 'Robert',
-        age: 23
-    }, (err, result) => {
-        if (err) {
-            return console.log('There was an error adding the todo', err);
-        }
-    console.log(JSON.stringify(result.ops, undefined, 2));
-    });
-    // db.collection('Todos').find().count().then( (count)=> {
-    //     console.log(`Todos Count: ${count}`);
-    // }, (err) => {
-    //     console.log('Unable to fetch count', err);
+    // db.collection('Todos').insertOne({
+    //     text: 'Charge apple watch',
+    //     completed: false
+    // },(err, result) => {
+    //     if (err) {
+    //         return console.log('There was an error adding the todo', err);
+    //     }
+    // console.log(JSON.stringify(result.ops, undefined, 2));
     // });
+    // db.collection('Users').insertOne({
+    //     name: 'Robert',
+    //     age: 23
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('There was an error adding the todo', err);
+    //     }
+    // console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5c3e26be72664b171673e213')
+    // }).toArray().then((docs)=>{
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+
+    // }, (err) => {
+    //     console.log("Unable to fetch todos", err);
+    // });
+    db.collection('Todos').find().count().then( (count)=> {
+        console.log(`Todos Count: ${count}`);
+    }, (err) => {
+        console.log('Unable to fetch count', err);
+    });
     // db.collection('Users').find({name: 'Robert Checco'}).toArray().then((docs) => {
     //     console.log(JSON.stringify(docs, undefined, 2));
     // }, (err) => {
     //     return console.log('Count not find a user with that name');
     // })
-    db.close();
+    // db.close();
 });
---
+++ playgrounds/mongodb-delete.js
@@ -0,0 +1,19 @@
+const {MongoClient} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to mongo db server');
+    }
+    console.log('Connected to the MongoDB server');
+
+    // deleteMany
+    db.collection('Todos').deleteMany({text: "Eat Lunch"}).then((result) => {
+        console.log(result);
+    })
+    // deleteOne
+
+    // findOneAndDelete
+
+
+
+});
