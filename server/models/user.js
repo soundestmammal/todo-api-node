5f58a8b5d1b99fbb5f6f312c2f8aba25bd8109a1
Robert Checco <robert.checco@gmail.com>
2019-02-02 21:43:57 -0500

    Add email validation

--- server/models/user.js
+++ server/models/user.js
@@ -5,8 +5,28 @@ var User = mongoose.model('User', {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{Value} is not a valid email'
+        }
+    },
+    password: {
+        type: string,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: string,
+            required: true
+        },
+        token: {
+            type: string,
+            require: true
+        }
+    }]
 });
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -52,6 +52,30 @@ app.get('/todos/:id', (req, res) => {
     }).catch( (e) => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send(todo);
+    }).catch((e) => res.status(400).send());
+})
+
+app.post('/login', (req, res) => {
+    var User = new User({
+        email: 'rob@che.com',
+        password: 'this490382hf',
+
+    })
+})
+
+ 
+
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
